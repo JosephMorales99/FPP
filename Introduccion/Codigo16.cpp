f7ba0f06c07e765a5e919c9cad1557c5ffbdcf55
JosephMorales99 <jj.ma94@gmail.com>
2024-11-25 11:47:41 -0600

    Create Codigo16.cpp

---
+++ Introduccion/Codigo16.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int numeros[10]{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+	vector <int> vnum
+	{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+	int mayor = -1;
+	for (int i = 0; i <= 9; i++)
+	{
+		if (vnum[i])> mayor)
+		{
+			mayor = numeros[i];
+		}
+	}
+	cout << "El mayor es: " << mayor << endl;
+}
