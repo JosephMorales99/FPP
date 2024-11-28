d691e59856e8683767e529a5a2c5fc323d4144b5
JosephMorales99 <jj.ma94@gmail.com>
2024-11-28 11:33:00 -0600

    Create Codigo18.cpp

---
+++ Introduccion/Codigo18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Funciones
+void hola()
+{
+	cout << "Hola mundo";
+}
+//No puedo definir funciones dentro de otra funcion 
+int main() //Punto de entrada
+{
+	hola(); //Esto es invocar o llamar la funcion 
+}
+
