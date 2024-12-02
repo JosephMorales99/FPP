1cd5a382b6ee58cbd25f348165bcbf7e93707b18
JosephMorales99 <jj.ma94@gmail.com>
2024-12-02 11:58:44 -0600

    Create Codigo21.cpp

---
+++ Introduccion/Codigo21.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct Alumno {
+    string nombre;
+    // Atributos o propiedades
+    int cuenta;
+};
+
+int main()
+{
+    Alumno jose;
+    jose.nombre = "Jose";
+    jose.cuenta = 123435;
+
+    Alumno polet =
+        polet.nombre = 335013;
+    cout << " nombre: " << jose.nombre << endl;
+    cout << " cuenta: " << jose.cuenta << endl;
+    cout << endl;
+    cout << " nombre: " << polet.nombre << endl;
+}
